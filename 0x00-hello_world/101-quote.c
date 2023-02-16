b2111b198f770226e1fb756207c95ed852605b50
Scrappycow <scrivon@gmail.com>
2023-02-16 15:47:16 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * 
+ * Description: "and that piece of art is beautiful" - Dora Korpar, 2015-10-19
+ *
+ * Return: 1 (Fail)
+ */
+int main(void)
+{
+	printf(stderr, "\"and that piece of art is beautiful" - Dora Korpar, 2015-10-19\n")
+	return (1);
+}
