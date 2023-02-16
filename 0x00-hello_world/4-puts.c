1c0569dc7abd11aec90f65bb397de60cafe1b8bd
Scrappycow <scrivon@gmail.com>
2023-02-16 12:52:34 -0800

    using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - a program that prints the desired txt using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("programming is like building a multiligual puzzle");
+
+	return (0);
+}
