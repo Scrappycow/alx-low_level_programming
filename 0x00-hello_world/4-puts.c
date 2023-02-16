37c3f4d510eadf2fdb66c0267d9c646e0cbbf5ce
Scrappycow <scrivon@gmail.com>
2023-02-16 13:21:40 -0800

    puts part 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-/*
- * main - a program that prints the desired txt using puts function
+#include <stdio.h>
+/**
+ * main - Entry point of the program
  * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("programming is like building a multiligual puzzle");
+	puts("\"programming is like building a multiligual puzzle");
 
 	return (0);
 }
