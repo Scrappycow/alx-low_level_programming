44963d33829edd4fb0776dabbeb28024445661dc
Scrappycow <scrivon@gmail.com>
2023-02-16 14:18:14 -0800

    crying

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	printf("\n Size of a char : % lu, sizeof(char));
+	printf("\n Size of a int : % lu, sizeof(int));
+	printf("\n Size of a long int : % lu, sizeof(long));
+	printf("\n Size of a long long int : % lu, sizeof(long long));
+	printf("\n Size of a float : % lu, sizeof(float));
+	return (0);
+}
