300460f3b52ea65d8ec0df4582b84162c365453d
Scrappycow <scrivon@gmail.com>
2023-02-17 09:10:49 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,33 @@
+#include <stdioh>
 #include <stdlib.h>
+#include <conio.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description - checks the last digit of a code againstfive
+ *
+ * Return: Always 0 (Success)
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n>5)
+	last_dig = n % 10;
+
+	if (n > 5)
 	{
-		printf("Last digit of %d\n is %d/n and is greater than 5")
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
-		printf("Last digit of %d\n is 0 and is 0")
+		printf("Last digit of %d is %d and is 0\n", n, last_dig);
 	}
 	else
 	{
-		printf("LAst digit of %d\n is less than 6 and not 0")
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
 	return (0);
 }
