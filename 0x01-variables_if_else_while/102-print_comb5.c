4cf70f21c8ca9b622019411bdc0b257a513c26c1
Scrappycow <scrivon@gmail.com>
2023-02-17 13:29:57 -0800

    advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description - prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int firstDig = o, secDig;
+
+	while (firstDig <= 99)
+	{
+		secDig = firstDig;
+		while (secDig <= 99)
+		{
+			if (secDig != firstDig)
+			{
+				putchar((firstDig / 10) + 48);
+				putchar((firstDig % 10) + 48);
+				putchar(' ');
+				putchar((secDig / 10) + 48);
+				putchar((secDig % 10) + 48);
+
+				if (firstDig != 98 || secDig != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++secDig;
+		}
+		++firstDig;
+	}
+	putchar('\n');
+
+	return (0);
+}
