04e5da07d41a01e9bc785951687ad1c02d52332d
Scrappycow <scrivon@gmail.com>
2023-02-17 10:46:54 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description - function prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(tolower(x));
+	return (0);
+}
