6fb8b5a179c8c60f81496ca8e3421cb32da14193
Scrappycow <scrivon@gmail.com>
2023-02-21 15:14:43 -0800

    10 and 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a1: first num
+ * @a2: second num
+ *
+ * Return: a1 + a2
+ */
+int add(int a1, int a2)
+{
+	return (a1 + a2);
+}
