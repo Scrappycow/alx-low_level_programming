cc73ab5a61c12b2068801c5a86e0ca49724f71c5
Scrappycow <scrivon@gmail.com>
2023-02-21 10:27:56 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - prints the alphabet x10 in lower case
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char ch;
+
+	while (count <= 9; count++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	_putchar('\n');
+	}
+}
