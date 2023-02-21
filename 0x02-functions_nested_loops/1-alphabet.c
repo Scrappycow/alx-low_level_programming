f42563b26553e5fa962322c912b1899ce7b581b6
Scrappycow <scrivon@gmail.com>
2023-02-21 10:07:37 -0800

    corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main -prints the alphabet in lower case
+ * print_alphabet - in lower case
  *
- * return: 0
  */
+
 void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
 	_putchar('\n');
-	return (0);
 }
