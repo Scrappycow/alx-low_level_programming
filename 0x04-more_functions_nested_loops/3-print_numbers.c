812ec3e33233908a743e008abd466df040e9bf65
Scrappycow <scrivon@gmail.com>
2023-02-23 02:03:32 -0800

    3 corrs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - check the code
+ * print_numbers - prints the numbers 0 to 9
  * Return: Always 0.
  */
 
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i < 57; i++)
 	{
-		_putchar(n);
+		_putchar(i);
 	}
 	_putchar('\n');
 	return (0);
