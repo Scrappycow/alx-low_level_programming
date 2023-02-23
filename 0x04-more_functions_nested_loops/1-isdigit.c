cf83107c4c98381bc4713b58c681f7120f1e3d2f
Scrappycow <scrivon@gmail.com>
2023-02-23 01:03:32 -0800

    corres

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,23 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * main - check the code
- * @c: parameter being checked
- * Return: Always 0.
- */
-int _isdigit(int c);
-
-int main(void)
-{
-	char c;
-
-	c = '0';
-	printf("%c: %d\n", c, _isdigit(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isdigit(c));
-	return (0);
-}
 /**
  * _isdigit - checks if character is a digit
  * @c: parameter being checked
