a14402c69b6c0f2d70ef291ca9a3e8dd2bc74cbe
Scrappycow <scrivon@gmail.com>
2023-02-22 23:40:49 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * @c: parameter being chacked
+ * Return: Always 0.
+ */
+
+int _isupper(int c);
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
+
+/**
+ * _isupper - checks for uppercase
+ * @c: parameter being chacked
+ * Return: 1 if char is lowercase, otherwise 0
+ */
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	else
+		return (0);
+	}
+}
