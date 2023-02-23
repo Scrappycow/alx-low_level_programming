22720042dec9a3c56d26c3ae5555f26a8ed2cc34
Scrappycow <scrivon@gmail.com>
2023-02-23 01:46:47 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n >= 0 && n <= 9)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
