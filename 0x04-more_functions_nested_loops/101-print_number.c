1befa9e6660c3ccbf1fe6c47d15f96e82525d85c
Scrappycow <scrivon@gmail.com>
2023-02-23 15:42:38 -0800

    adv

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * Description: prints number
+ * @n: parameter being tested
+ *
+ * Return: Always 0.
+ */
+void print_number(int n);
+/**
+ * print_number - prints a number
+ * Description: prints number
+ * @n: parameter being tested
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int new_num;
+
+	if (n < 0)
+	{
+		new_num = -n;
+		_putchar('_');
+	}
+	else
+	{
+		new_num = n;
+	}
+
+	if ( new_num / 10)
+	{
+		print_number (new_num / 10);
+	}
+	_putchar((new_num % 10) + '0');
+}
