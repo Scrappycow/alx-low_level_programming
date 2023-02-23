9af9ca7cd1465727d5353af6f9c55579cb996180
Scrappycow <scrivon@gmail.com>
2023-02-23 01:21:15 -0800

    task 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * mul - multiplies the input
+ * @a: parameter being multiplied
+ * @b: parameter being mulltiplied
  *
  * Return: Always 0.
  */
-int main(void)
+
+int mul(int a, int b)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+	int a;
+	int b;
+	int mul = a * b;
+
+	return (mul);
 }
