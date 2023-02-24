80c0854444bad358fe2da07d2887ce2fdee57abf
Scrappycow <scrivon@gmail.com>
2023-02-24 10:51:36 -0800

    trying

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,30 +10,23 @@ int main(void)
 {
 	int i;
 
-	i = 1;
-
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("%s", "Fizz ");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf("%s", "Buzz ");
-		}
-		else if (i % 5 == 0 && i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("%s", "FizzBuzz ");
 		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz ");
+		}
 		else
 		{
-			if (i > 9 && i <= 99)
-			{
-				putchar(i / 10 + '0');
-			}
-			putchar(i % 10 + '0');
-			putchar(' ');
+			printf("%d", i);
 		}
 	}
 	putchar('\n');
