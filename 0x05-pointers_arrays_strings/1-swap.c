5e8b00fdd26a6acd86870cbbd41093d5f055f350
Scrappycow <scrivon@gmail.com>
2023-02-27 09:44:40 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: new value of a
+ * @b: new value of b
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
