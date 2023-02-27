1cf7b77f7ac1575d3c32a3b00dbd3eb6b6c91b5c
Scrappycow <scrivon@gmail.com>
2023-02-27 09:02:41 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: parameter being tested
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	int n = 402;
+	int *p;
+
+	p = &n;
+	*p = 98;
+	return (0);
+}
