e612c3b87919573b10c794939c701a598532c662
Scrappycow <scrivon@gmail.com>
2023-03-14 11:33:07 -0700

    links

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,29 +15,17 @@ int main(int argc, char *argv[])
 	int i, j, t;
 	int sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
 	for (i = 1 ; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		int t = atoi(argv[i]);
-
-		if (t < 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += t;
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
