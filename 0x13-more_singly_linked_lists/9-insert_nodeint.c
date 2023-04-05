6be8f76dbec524a503dcf1d795c4834070f22657
Scrappycow <scrivon@gmail.com>
2023-04-05 12:54:09 -0700

    task 9 more corrs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (cat == NULL)
 			return (NULL);
 	}
-	new_node1 = malloc(sizzeof(listint_t));
+	new_node1 = malloc(sizeof(listint_t));
 	if (new_node1 == NULL)
 		return (NULL);
 	new_node1->n = n;
