96493ce68dc79c89c0a2d83b6a026d625f46692f
Scrappycow <scrivon@gmail.com>
2023-04-05 12:48:43 -0700

    3rd trial

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node1, cat;
-	int unsigned int i = 0;
+	unsigned int i = 0;
 
 	if (**head == NULL && idx != 0)
 		return (NULL);
