1fec78b390078153dc035c9203ff92943a0865cf
Scrappycow <scrivon@gmail.com>
2023-04-05 12:40:56 -0700

    task 9 correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,3 +14,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *cat;
 	unsigned int i = 0;
+
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	if (idx != 0)
+	{
+		cat = *head;
+		for (; i < idx - 1 && cat != NULL; i++)
+			cat = cat->next;
+		if (cat == NULL)
+			return (NULL);
+	}
+	new_node1 = malloc(sizzeof(listint_t));
+	if (new_node1 == NULL)
+		return (NULL);
+	new_node1->n = n;
+	if (idx == 0)
+	{
+		new_node1->next = *head;
+		*head = new_node1;
+		return (new_node1);
+	}
+	new_node1->next = cat->next;
+	cat->next = new_node1;
+	return (new_node1);
+}
