b3ff753d4f79b6f8cf54ba0875c8b5471a8b110e
Scrappycow <scrivon@gmail.com>
2023-06-09 22:59:10 -0700

    click

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,53 +1,55 @@
-#include "hash_tables.h"
+include "hash_tables.h"
+
+hash_node_t *create_new_node(const char *key, const char *value);
+
 /**
  * hash_table_set - adds an element to hash table.
  * @ht: hash table
  * @key: is the key. Not an empty string
  * @value: value associated with the key.
  * value that must be duplicated. value can be an empty string
- * Return: 1 on success, 0 on failurre
+ * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *node;
+	hash_node_t *new_node;
+	unsigned long int index;
 
-hash_node_t *node;
-hash_node_t *new_node;
-unsigned long int index;
+	if (ht == NULL || *key == '\n' || *value == '\n')
+		return (0);
 
-if (ht == NULL || *key == '\n' || *value == '\n')
-	return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-index = key_index((const unsigned char *)key, ht->size);
-node = ht->array[index];
+	if (node == NULL)
+	{
+		new_node = create_new_node(key, value);
+		if (new_node == NULL)
+			return (0);
 
-if (node == NULL)
-{
+		ht->array[index] = new_node;
+		return (1);
+	}
+
+	while (node != NULL)
+	{
+		if (strcmp(key, node->key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+
 	new_node = create_new_node(key, value);
 	if (new_node == NULL)
 		return (0);
 
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
-}
-
-while (node != NULL)
-{
-	if (strcmp(key, node->key) == 0)
-	{
-		free(node->value);
-		node->value = strdup(value);
-		return (1);
-	}
-	node = node->next;
-}
-
-new_node = create_new_node(key, value);
-if (new_node == NULL)
-	return (0);
-
-new_node->next = ht->array[index];
-ht->array[index] = new_node;
-return (1);
 }
 
 /**
@@ -58,7 +60,7 @@ return (1);
  * Return: 1 on success, 0 on failurre
  */
 
-hash_node_t *create_new_node (const char *key, const char *value)
+hash_node_t *create_new_node(const char *key, const char *value)
 {
 	hash_node_t *new_node;
 
