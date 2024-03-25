8394609d2e5f20476cc5870f20985add573e43c2
Scrappycow <scrivon@gmail.com>
2024-03-25 14:03:40 -0700

    tasks

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -41,7 +41,7 @@ int recursive_search(int *array, size_t size, int value)
 
 /**
  * binary_search - calls to binary_search to return
- * the index of number
+ * index of the number
  *
  * @array: input array
  * @size: size of the array
@@ -58,4 +58,46 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 
 	return (index);
+}
+
+/**
+ * exponential_search - searches for a value in an array of
+ * integers using exponential search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t index, next;
+	int result;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] == value)
+		return (0);
+
+	index = 1;
+
+	while (array[index] < value && index < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)index, array[index]);
+		index *= 2;
+	}
+
+	next = (index >= size) ? (size - 1) : index;
+
+	index /= 2;
+
+	printf("Value found between indexes [%d] and [%d]\n", (int)index, (int)next);
+
+	result = binary_search(array + index, (next + 1) - index, value);
+
+	if (result >= 0)
+		result += index;
+
+	return (result);
 }
